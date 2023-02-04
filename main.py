94ab778d6b252bbd8e4fd7a9acee20244126cfd2
Gabriel <gabrielotta2013@gmail.com>
2023-02-03 22:28:30 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from time import sleep
+import speech_recognition as sr
+import keyboard  
+import va
+
+recognizer = sr.Recognizer()
+
+BOT_NAMES = ['marvin', 'marlin', 'mavin', 'marli', 'martin', "mommy" ]
+
+def record_audio():
+  with sr.Microphone() as source:
+    audio = recognizer.listen(source, None, 3)
+    voice_data = ''
+    try:
+      voice_data = recognizer.recognize_google(audio, language="pt-BR").lower()
+    except sr.UnknownValueError:
+      print()
+    except sr.RequestError:
+      print('Serviço offline')
+
+    print(">>", voice_data)
+    for bot_name_variation in BOT_NAMES:
+      if (bot_name_variation in voice_data):
+        va.listen()
+        break
+
+while True:
+  sleep(0.05) 
+  if keyboard.is_pressed('F8'):
+    print('ouvindo...')
+    va.listen()
+  sleep(0.05)
---
+++ modules/OpenModule.py
@@ -0,0 +1,30 @@
+import webbrowser
+import subprocess
+import va
+
+def parse_command(command_action, argument=""):
+  if command_action[0:4]=="web!":
+    open_link(command_action[5:])
+  elif command_action[0:4]=="web?":
+    open_link_with_search(command_action[5:], argument)
+    va.speak("Pesquisando " + argument)
+  elif command_action[0:4]=="path":
+    open_program(command_action[5:])
+    va.speak("Abrindo " + argument)
+  elif command_action=='commands':
+    reload_commands()
+    va.speak("Atualizado")
+
+
+def open_link_with_search(link, argument=""):
+  print(f'{link}/search?q={argument}')
+  webbrowser.get('windows-default').open_new(f'{link}/search?q={argument}')
+
+def open_link(link):
+  webbrowser.get('windows-default').open_new(f'{link}')
+
+def open_program(path):
+  subprocess.Popen([path, '-new-tab'])
+
+def reload_commands():
+  va.read_commands_file()
